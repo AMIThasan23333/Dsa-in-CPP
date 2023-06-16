765387e103e002d5fdf084b2bae998033824f576
My Name <cpgsom819@gmail.com>
2023-06-16 11:41:30 +0800

    vector initialization done

---
+++ STL/type2.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+/* 
+TYPE 2 
+if no value are given than it will be 0 by default . if give value than that will be showed  */
+
+      vector<int> v(5);
+
+      for (size_t i = 0; i <v.size(); i++)
+
+      {
+        cout << v[i] << " ";
+      }
+
+      cout << v.size() << endl;
+      return 0;
+
+      
+
+
+return 0;
+}
---
+++ STL/vectorinitia.cpp
@@ -0,0 +1,14 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    
+     vector<int> v;   /* type 1 */
+ 
+      cout << v.size()<< endl;
+
+return 0;
+}
