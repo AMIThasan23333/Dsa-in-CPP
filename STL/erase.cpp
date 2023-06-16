641f54927f7c15b2675b108c19d73d2a10c0f934
My Name <cpgsom819@gmail.com>
2023-06-16 18:30:44 +0800

    replace

--- STL/erase.cpp
+++ STL/erase.cpp
@@ -5,8 +5,10 @@ int main(){
 
     vector<int> v={1,2,3,4,5};
 
-    v.erase(v.begin() + 3);
+    // v.erase(v.begin() + 3);
 
+    /* delete multiple */
+   v.erase(v.begin() + 1 , v.begin()+ 4);
     for (int  x :v)
     {
         cout << x <<" ";
---
+++ STL/replace.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+
+vector<int> v= {1,2,3,4,5,6,7,2,2,2,2};
+
+replace(v.begin(), v.end(), 2,100);
+
+  for (int x: v)
+  {
+     cout << x << " ";
+  }
+  
+
+return 0;
+}
